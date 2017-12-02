63a78c76ed1fc30a73b18a621beac9619cf71319
jcrjaci <jacinto_rodrigues1@hotmail.com>
2017-12-02 22:55:44 +0000

    add uglify

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const webpack = require('webpack');
 const path = require('path');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 let config = {
 
   entry: './src/index.js', // entry file
@@ -26,7 +28,8 @@ let config = {
     ] // end rules
   },
   plugins: [
-    new ExtractTextWebpackPlugin('styles.css') // call the ExtractTextWebpackPlugin constructor and name our css file
+    new ExtractTextWebpackPlugin('styles.css'), // call the ExtractTextWebpackPlugin constructor and name our css file
+    new webpack.optimize.UglifyJsPlugin() // call th uglify plugin
   ],
   devServer: {
     contentBase: path.resolve(__dirname, './public'), // A directory or URL to serve HTML content from.
