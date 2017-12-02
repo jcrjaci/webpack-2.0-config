350ee70e6f76baba449e9179a863cde1cc9116df
jcrjaci <jacinto_rodrigues1@hotmail.com>
2017-12-02 22:25:30 +0000

    add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 const config = {
-	entry: './index.js',
-	output: {
-		filename: 'output.js'
-	},
-module: {
-    rules: [{
-      test: /\.js$/, // files ending with .js
-      exclude: /node_modules/, // exclude the node_modules directory
-      loader: "babel-loader" // use this (babel-core) loader
-    }]
+  entry: "./index.js",
+  output: {
+    filename: "output.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // files ending with .js
+        exclude: /node_modules/, // exclude the node_modules directory
+        loader: "babel-loader" // use this (babel-core) loader
+      },
+      {
+        test: /\.scss$/, // files ending with .scss
+        loader: ["style-loader", "css-loader", "sass-loader"] // use these loaders
+      }
+    ]
+  },
+  stats: {
+    colors: true
   }
 };
 
-module.exports = config; 
+module.exports = config;
